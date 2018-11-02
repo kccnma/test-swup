fad3bcfce7ac3730e7dfe5d7417d1d94cd3e3472
kccnma <gargiulo@hawaii.edu>
2018-11-01 14:25:01 -1000

    added initial files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # test-swup
+
 Page transition test using Swup
+
+[View Demo](https://kccnma.github.io/test-swup)
---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // SWUP: Initial Animate in
+  setTimeout(function() {
+    document.documentElement.classList.remove("is-animating");
+  }, 100);
+
+  // SWUP: enable swup
+  const swup = new Swup();
+});
