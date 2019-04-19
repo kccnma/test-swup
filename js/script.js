aee7de6284170ed1ac3011d563be2f2cbcf70fa2
kccnma <gargiulo@hawaii.edu>
2019-04-18 22:55:02 -1000

    fixed broken swup, fade out now working

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // SWUP: Initial Animate in
-  setTimeout(function() {
-    document.documentElement.classList.remove("is-animating");
-  }, 100);
+document.addEventListener("DOMContentLoaded", function () {
 
   // SWUP: enable swup
-  const swup = new Swup();
+  let options = {
+    LINK_SELECTOR: 'a[href^="' + window.location.origin + '"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup])',
+    FORM_SELECTOR: 'form[data-swup-form]',
+    elements: [
+      '#swup'
+    ],
+    animationSelector: '[class*="transition-"]',
+    cache: true,
+    pageClassPrefix: '',
+    scroll: true,
+    debugMode: true,
+    preload: true,
+    support: true,
+    skipPopStateHandling: function (event) {
+      if (event.state && event.state.source == "swup") {
+        return false;
+      }
+      return true;
+    },
+    animateHistoryBrowsing: false,
+  }
+  const swup = new Swup(options);
+
+  swup.on('animationOutDone', function () {
+    console.log("leave is done");
+  });
+
 });
